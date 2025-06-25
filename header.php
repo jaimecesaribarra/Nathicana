7501a8e17df322f7c65d0b7b5d5a0e8b458a1395
jaimecesaribarra <jaimecesaribarra@gmail.com>
2025-06-24 19:44:00 -0600

    Armando Partes

---
+++ header.php
@@ -0,0 +1,13 @@
+<header>  
+    <img src="" alt="">
+        <nav>
+            <ul>
+                <li>
+                    <a class="active" href="/">Inicio</a>
+                    <a href="/nathicana">Nathicana</a>
+                    <a href="/portafolio">Portafolio</a>
+                    <a href="/contacto">Contacto</a>
+                </li>
+            </ul>
+        </nav>
+</header>  
--- main.php
+++ main.php
@@ -3,9 +3,18 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/header.css">
+    <script src="/js/main.js"></script>
 	<title>Nathicana</title>
 </head>
 <body>
-	
+	<section>
+		<h1>Nathicana</h1>
+	</section>
+
+	<?php
+        include 'footer.php';
+    ?>
 </body>
 </html>
